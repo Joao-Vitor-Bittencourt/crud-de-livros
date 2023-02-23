975e9d77f6e02e48268d1704ca4ac2dda9caf929
João Vítor Bittencourt Barbosa <joaoestudantepeog@gmail.com>
2023-02-23 11:46:33 -0300

    :construction: creating a routes file

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const routes = require('./routes.js');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.use(routes);
 
-app.listen(port, () => {
-  console.log('Server is running on http://localhost:3000');
-})
+app.listen(3000 , () => console.log('Server is running on http://localhost:3000'));
