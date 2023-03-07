078624eec0ba305576b757887e37b36e1d7c97cf
João Vítor Bittencourt Barbosa <joaoestudantepeog@gmail.com>
2023-03-07 13:03:03 -0300

    adding new routes

--- api/src/routes.js
+++ api/src/routes.js
@@ -7,5 +7,7 @@ const router = Router();
 router.get('/livros', BooksController.index);
 router.get('/livros/:id', BooksController.show);
 router.post('/livros', BooksController.store);
+router.put('/livros/:id', BooksController.update);
+router.delete('livros/:id', BooksController.delete);
 
 module.exports = router;
